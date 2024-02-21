8cbf259eb8973eccde700e58512437d5038e307c
rokopluk <130516150+rokopluk@users.noreply.github.com>
2024-02-21 06:58:03 +0000

    Fixed bob to be uppercase

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add JavaScript below
 function randomNameGenerator() {
     return{
-        names:['Billy','Joe','John','Steve','Jennifer','Rico','bob','Jeffrey','Alex','Ben'],
+        names:['Billy','Joe','John','Steve','Jennifer','Rico','Bob','Jeffrey','Alex','Ben'],
         randomName:'',
         generateRandomName(){
             this.randomName = this.names[Math.floor(Math.random()*this.names.length)];
