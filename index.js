0c4aca3350deb971235b37a8c7cd2a6a99d7d6df
rokopluk <130516150+rokopluk@users.noreply.github.com>
2024-02-21 06:56:33 +0000

    Working name generator

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add JavaScript below
+function randomNameGenerator() {
+    return{
+        names:['Billy','Joe','John','Steve','Jennifer','Rico','bob','Jeffrey','Alex','Ben'],
+        randomName:'',
+        generateRandomName(){
+            this.randomName = this.names[Math.floor(Math.random()*this.names.length)];
+        }
+    };
+}
